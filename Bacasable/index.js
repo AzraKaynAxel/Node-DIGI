905e878b6049f8b2e4ebb1e79973d9d1278cfbea
dinel <rddinel@gmail.com>
2026-01-16 16:07:38 +0100

    Exemple serveur node

--- Bacasable/index.js
+++ Bacasable/index.js
@@ -1,1 +1,17 @@
-console.log("Hello Worlddd");
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/toto') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Hello toto');
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
+});
+
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000/');
+})
+
+// Aller voir comment fonctionnent les objets REQ et RES de HTTP et ce qu'on peut faire pour l'exercice
